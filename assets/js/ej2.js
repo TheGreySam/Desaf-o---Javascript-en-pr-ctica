c9cb5d308db783cea8fc24181f13e6df5f175891
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-07-08 19:04:31 -0400

    second commit

---
+++ assets/js/ej2.js
@@ -0,0 +1,46 @@
+//function color1(backgroundColor) {
+  //document.getElementById('caja').style.backgroundColor = 'red';
+
+//}
+// function hello() {
+// alert('Hello');
+// }
+// document.getElementById("btn1").addEventListener("click", hello);
+// function color1() {
+//   document.getElementById('caja').style.backgroundColor = 'blue'
+// }
+//document.getElementById("caja").style.backgroundColor = "blue";
+
+// var button = document.querySelector('btn1');
+//
+// button.onclick = function () {
+//  var red = Math.floor(Math.random() * 256);
+//  var blue = Math.floor(Math.random() * 256);
+//  var green = Math.floor(Math.random() * 256);
+//
+//  this.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+// };
+
+var color1 = "btn-1";
+var color2 = "btn-2";
+var color3 = "btn-3";
+var color4 = "btn-4";
+var color5 = "btn-5";
+var color6 = "btn-6";
+
+function changeColor(color) {
+   document.getElementById('caja').style.background = color;
+};
+//
+// var button = document.querySelector("btn-1");
+//
+// button.addEventListener("click", function() {
+//     const curColour = document.body.style.backgroundColor;
+//
+//     if (curColour === 'red') {
+//         document.body.style.backgroundColor = "blue";
+//     }
+//     else {
+//         document.body.style.backgroundColor = "red";
+//     }
+// })();
---
+++ assets/js/ej3.js
@@ -0,0 +1,4 @@
+function suma(){
+  resultadosuma = parseInt(num1) + parseInt(num2);
+  document.getElementById("result").innerHTML = resultadosuma;
+};
