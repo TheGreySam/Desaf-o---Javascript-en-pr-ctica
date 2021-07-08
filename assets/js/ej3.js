2a4659987f164e7fe1b3d4e5948ff7f8ad235ede
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-07-08 19:51:13 -0400

    third commit

--- assets/js/ej3.js
+++ assets/js/ej3.js
@@ -1,4 +1,29 @@
-function suma(){
-  resultadosuma = parseInt(num1) + parseInt(num2);
+// var num1 = document.getElementById("valor1").value;
+// var num2 = document.getElementById("valor2").value;
+
+// function suma(){
+//   resultadosuma = parseInt(num1) + parseInt(num2);
+//   document.getElementById("result").innerHTML = resultadosuma;
+// };
+//
+// function resta(){
+//   resultadoresta = parseInt(num1) - parseInt(num2);
+//   document.getElementById("result").innerHTML = resultadoresta;
+// };
+function suma() {
+  var num1 = document.getElementById("valor1").value;
+  var num2 = document.getElementById("valor2").value;
+  var resultadosuma = parseInt(num1) + parseInt(num2);
   document.getElementById("result").innerHTML = resultadosuma;
 };
+
+function resta() {
+  var num1 = document.getElementById("valor1").value;
+  var num2 = document.getElementById("valor2").value;
+  var resultadoresta = parseInt(num1) - parseInt(num2);
+  document.getElementById("result").innerHTML = resultadoresta;
+  if (resultadoresta <= 0) {
+    document.getElementById("result").innerHTML = 0
+  };
+
+}
